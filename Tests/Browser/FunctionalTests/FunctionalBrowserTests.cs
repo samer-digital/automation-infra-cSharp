90d5e2f050eb8dc4fbdaefb8deb129bf23208f1d
samershareh <samer@digital-oneti.tech>
2024-06-15 23:44:30 +0300

    disable test

--- Tests/Browser/FunctionalTests/FunctionalBrowserTests.cs
+++ Tests/Browser/FunctionalTests/FunctionalBrowserTests.cs
@@ -40,7 +40,7 @@ public class FunctionalBrowserTests : BaseTest
         Assert.That(numOfDirections, Is.GreaterThanOrEqualTo(2), "Number of routes are less then the expected");
     }
 
-    [Test, Description("Test - Copy the address link and navigate")]
+    // [Test, Description("Test - Copy the address link and navigate")]
     public async Task CopyAddressLinkAndNavigateTest()
     {
 
